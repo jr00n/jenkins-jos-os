879370ab9f6c27946d4e430370f01f81cd4d5c65
Jeroen Wolff <jeroen.wolff@gmail.com>
2016-12-22 14:17:43 +0100

    Script changed so an ENV variable JNLP_SERVICE_NAME can be used in the DeploymentConfig to specifie the service name for the Jenkins master/slave communication

--- jenkins-swarm-slave-maven33/jenkins-slave.sh
+++ jenkins-swarm-slave-maven33/jenkins-slave.sh
@@ -4,39 +4,31 @@ master_username=${JENKINS_USERNAME:-"admin"}
 master_password=${JENKINS_PASSWORD:-"password"}
 slave_executors=${EXECUTORS:-"1"}
 
+#What is the OpenShift service name voor the master/slave communication
+#JNLP_SERVICE_NAME is the enviroment variable specified in the DeploymentConfig
+#if not specified "JENKINS_JNLP" will be used.
+JNLP_SERVICE_NAME=${JNLP_SERVICE_NAME:-JENKINS_JNLP}
+#convert to uppercase and replace '-' with '_'
+JNLP_SERVICE_NAME=`echo ${JNLP_SERVICE_NAME} | tr '[a-z]' '[A-Z]' | tr '-' '_'`
+T_HOST=${JNLP_SERVICE_NAME}_SERVICE_HOST
+# the '!' handles env variable indirection so we can resolve the nested variable
+# see: http://stackoverflow.com/a/14204692
+JNLP_HOST=${!T_HOST}
+T_PORT=${JNLP_SERVICE_NAME}_SERVICE_PORT
+JNLP_PORT=${!T_PORT}
+
+export JNLP_PORT=${JNLP_PORT:-50000}
 
 
-# If JENKINS_SECRET and JENKINS_JNLP_URL are present, run JNLP slave
-if [ ! -z $JENKINS_SECRET ] && [ ! -z $JENKINS_JNLP_URL ]; then
+echo "Running Jenkins Swarm Plugin...."
 
-    echo "Running Jenkins JNLP Slave...."
-	JAR=`ls -1 /opt/jenkins-slave/bin/slave.jar | tail -n 1`
+# jenkins swarm slave
+JAR=`ls -1 /opt/jenkins-slave/bin/swarm-client-*.jar | tail -n 1`
 
-	# if -tunnel is not provided try env vars
-	if [[ "$@" != *"-tunnel "* ]]; then
-		if [[ ! -z "$JENKINS_TUNNEL" ]]; then
-			TUNNEL="-tunnel $JENKINS_TUNNEL"
-		fi
-	fi
-
-	if [[ ! -z "$JENKINS_URL" ]]; then
-		URL="-url $JENKINS_URL"
-	fi
-
-	exec java $JAVA_OPTS -cp $JAR hudson.remoting.jnlp.Main -headless $TUNNEL $URL -jar-cache $HOME "$@"
-
-elif [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
-
-  echo "Running Jenkins Swarm Plugin...."
-
-  # jenkins swarm slave
-  JAR=`ls -1 /opt/jenkins-slave/bin/swarm-client-*.jar | tail -n 1`
-
-  if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
-	PARAMS="-master http://${JENKINS_SERVICE_HOST}:${JENKINS_SERVICE_PORT}${JENKINS_CONTEXT_PATH} -tunnel ${JENKINS_SLAVE_SERVICE_HOST}:${JENKINS_SLAVE_SERVICE_PORT}${JENKINS_SLAVE_CONTEXT_PATH} -username ${master_username} -password ${master_password} -executors ${slave_executors}"
-  fi
-
-  echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
-  exec java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
-
+if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_PORT_8080_TCP_ADDR" ]; then
+	#PARAMS="-master http://${JENKINS_SERVICE_HOST}:${JENKINS_SERVICE_PORT}${JENKINS_CONTEXT_PATH} -tunnel ${JENKINS_SLAVE_SERVICE_HOST}:${JENKINS_SLAVE_SERVICE_PORT}${JENKINS_SLAVE_CONTEXT_PATH} -username ${master_username} -password ${master_password} -executors ${slave_executors}"
+  PARAMS="-master http://${JENKINS_SERVICE_HOST}:${JENKINS_SERVICE_PORT} -tunnel ${JNLP_HOST}:${JNLP_PORT} -username ${master_username} -password ${master_password} -executors ${slave_executors}"
 fi
+
+echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
+exec java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
